447a7ace2cd664d1c263c2155b4070b7fa50087c
akanchha25 <akanchhakumari11@gmail.com>
2024-08-18 23:20:59 +0530

    dynamic attributes

--- utils/query_builder.go
+++ utils/query_builder.go
@@ -18,14 +18,38 @@ func BuildQuery(filters map[string]interface{}, sortFields map[string]string) *b
 
     // Add filters to the query
     for field, value := range filters {
-        query["query"].(map[string]interface{})["bool"].(map[string]interface{})["must"] = append(
-            query["query"].(map[string]interface{})["bool"].(map[string]interface{})["must"].([]interface{}),
-            map[string]interface{}{
+        switch v := value.(type) {
+        case map[string]interface{}: // Handle range queries
+            rangeQuery := map[string]interface{}{
                 "range": map[string]interface{}{
-                    field: value,
+                    field: v,
                 },
-            },
-        )
+            }
+            query["query"].(map[string]interface{})["bool"].(map[string]interface{})["must"] = append(
+                query["query"].(map[string]interface{})["bool"].(map[string]interface{})["must"].([]interface{}),
+                rangeQuery,
+            )
+        case float64, int: // Numeric fields
+            query["query"].(map[string]interface{})["bool"].(map[string]interface{})["must"] = append(
+                query["query"].(map[string]interface{})["bool"].(map[string]interface{})["must"].([]interface{}),
+                map[string]interface{}{
+                    "range": map[string]interface{}{
+                        field: map[string]interface{}{
+                            "gte": v,
+                        },
+                    },
+                },
+            )
+        case string: // Text or keyword fields
+            query["query"].(map[string]interface{})["bool"].(map[string]interface{})["must"] = append(
+                query["query"].(map[string]interface{})["bool"].(map[string]interface{})["must"].([]interface{}),
+                map[string]interface{}{
+                    "match": map[string]interface{}{
+                        field: v,
+                    },
+                },
+            )
+        }
     }
 
     // Add sorting to the query
